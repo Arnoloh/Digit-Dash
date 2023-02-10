09aacd05d7380d2b3f55f23b8dba461f39c929d2
Arnoloh's Mac <nono.corcione@gmail.com>
2023-02-10 16:24:47 +0100

    Update client file

--- Client/customer.c
+++ Client/customer.c
@@ -15,34 +15,32 @@
 void rewrite(int fd, const void *buf, size_t count)
 {
     ssize_t bytes_written = 0;
-    
+
     while ((size_t)bytes_written < count){
-    
+
         ssize_t current_write = write(fd, buf + bytes_written, count - bytes_written);
-        
+
         if (current_write < 0) {
             errx(3,"Error writing to file");
         }
-        
+
         bytes_written += current_write;
     }
-    
+
     return;
 }
 
 void client()
 {
-    int socketClient = socket(AF_INET, SOCK_STREAM, 0);
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addrClient;
-    addrClient.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addrClient.sin_addr.s_addr = inet_addr("82.65.173.135");
     addrClient.sin_family = AF_INET;
-    addrClient.sin_port = htons(30000);
-    connect(socketClient, (const struct sockaddr *)&addrClient, sizeof(addrClient));
-	printf("Client connected : %d\n", socketClient);
-	
-	char message2[24];
-	recv(socketClient, &message2, sizeof(message2), 0);
-	printf("%s\n", message2);
-	
-	close(socketClient);
+    addrClient.sin_port = htons(13080);
+    connect(sfd, (const struct sockaddr *)&addrClient, sizeof(addrClient));
+
+    char *message2="Do you copy ?\n";
+   write(sfd,message2,strlen(message2)); 
+
+    close(sfd);
 }
